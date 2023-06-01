72344a4bbf6a5b7a14edc4c3db05474e9108d76f
el-makarova <el-makarova@yandex-team.ru>
2023-06-01 17:47:38 +0300

    feat: add data source check health

---
+++ pkg/models/errors.go
@@ -0,0 +1,13 @@
+package models
+
+import "errors"
+
+var (
+	ErrInvalidJSON                               = errors.New("could not parse json")
+	ErrEndpointEmpty                             = errors.New("endpoint should not be empty")
+	ErrServiceAccAuthAccessKeyEmpty              = errors.New("service account auth access key should not be empty")
+	ErrAccessTokenEmpty              			 = errors.New("access token should not be empty")
+	ErrUserOrPasswordEmpty              		 = errors.New("username and password should not be empty")
+	ErrDBLocationEmpty                           = errors.New("data base location should not be empty")
+	ErrLoadingSettingsForServiceAccAuthAccessKey = errors.New("error loading settings for service account auth access key")
+)
