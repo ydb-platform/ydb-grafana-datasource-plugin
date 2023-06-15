b36153e7c27f6959824011304b93ac816688953f
el-makarova <el-makarova@yandex-team.ru>
2023-06-15 16:14:31 +0300

    feat: add query formats and tables fetching

---
+++ src/components/QueryEditor/YDBQueryEditor.tsx
@@ -0,0 +1,62 @@
+import { Button, Form } from '@grafana/ui';
+import { QueryEditorProps } from '@grafana/data';
+
+import { QueryTypeSwitcher } from './components/QueryTypeSwitcher';
+import { QueryBuilder } from './components/QueryBuilder';
+import { SqlEditor } from './components/SqlEditor';
+
+import { QueryType, YDBBuilderQuery, YDBQuery, YDBSQLQuery } from './types';
+import { YdbDataSourceOptions } from 'components/ConfigEditor/types';
+import { defaultYDBBuilderQuery, defaultYDBSQLQuery } from './constants';
+import { DataSource } from 'datasource';
+
+type YDBQueryEditorProps = QueryEditorProps<DataSource, YDBQuery, YdbDataSourceOptions>;
+
+function normalizeBuilderQuery(query: YDBBuilderQuery): YDBBuilderQuery {
+  return { ...defaultYDBBuilderQuery, ...query };
+}
+function normalizeSQLQuery(query: YDBSQLQuery): YDBSQLQuery {
+  return { ...defaultYDBSQLQuery, ...query };
+}
+
+function normalizeQuery(query: YDBQuery) {
+  if (query.queryType === 'sql') {
+    return normalizeSQLQuery(query);
+  }
+  return normalizeBuilderQuery(query);
+}
+
+export function YDBQueryEditor({ query: baseQuery, onChange, onRunQuery, datasource }: YDBQueryEditorProps) {
+  const query = normalizeQuery(baseQuery);
+  const { queryType } = query;
+
+  const handleChangeQueryAttribute = <T,>(value: Partial<T>) => {
+    onChange({ ...query, ...value });
+  };
+
+  const handleChangeQueryType = (type: QueryType) => {
+    handleChangeQueryAttribute<YDBQuery>({ queryType: type });
+  };
+
+  return (
+    <Form onSubmit={onRunQuery}>
+      {() => (
+        <>
+          <div className={'gf-form'}>
+            <QueryTypeSwitcher queryType={queryType} onChange={handleChangeQueryType} />
+            <Button type="submit">Run Query</Button>
+          </div>
+          {queryType === 'builder' ? (
+            <QueryBuilder
+              datasource={datasource}
+              query={query}
+              onChange={handleChangeQueryAttribute<YDBBuilderQuery>}
+            />
+          ) : (
+            <SqlEditor onChange={handleChangeQueryAttribute<YDBSQLQuery>} query={query} />
+          )}
+        </>
+      )}
+    </Form>
+  );
+}
---
+++ src/components/QueryEditor/types.ts
@@ -0,0 +1,43 @@
+import { DataQuery } from '@grafana/schema';
+
+export interface OnChangeQueryAttribute<T> {
+  (arg: Partial<T>): void;
+}
+
+export const QueryTypes = {
+  SQL: 'sql',
+  Builder: 'builder',
+} as const;
+
+export type QueryType = (typeof QueryTypes)[keyof typeof QueryTypes];
+
+export interface YDBQueryBase extends DataQuery {}
+
+export interface YDBSQLQuery extends YDBQueryBase {
+  queryType: typeof QueryTypes.SQL;
+  rawSql: string;
+  meta?: {
+    timezone?: string;
+    // meta fields to be used just for building builder options when migrating  back to QueryType.Builder
+    builderOptions?: SqlBuilderOptions;
+  };
+}
+
+export interface YDBBuilderQuery extends YDBQueryBase {
+  queryType: typeof QueryTypes.Builder;
+  rawSql: string;
+  builderOptions: SqlBuilderOptions;
+  meta?: {
+    timezone?: string;
+  };
+}
+
+export type YDBQuery = YDBSQLQuery | YDBBuilderQuery;
+
+export interface SqlBuilderOptionsList {
+  table?: string;
+  fields?: string[];
+  limit?: number;
+}
+
+export type SqlBuilderOptions = SqlBuilderOptionsList;
