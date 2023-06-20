a7bdb455165656c97f4b2ce82fc13a586ea084c0
el-makarova <el-makarova@yandex-team.ru>
2023-06-20 18:38:31 +0300

    feat: prepare raw query on builder options change

--- src/components/QueryEditor/helpers.ts
+++ src/components/QueryEditor/helpers.ts
@@ -1,4 +1,4 @@
-import { QueryFormat } from './types';
+import { QueryFormat, SqlBuilderOptions } from './types';
 
 export function ConvertQueryFormatToVisualizationType(format: QueryFormat) {
   switch (format) {
@@ -9,4 +9,21 @@ export function ConvertQueryFormatToVisualizationType(format: QueryFormat) {
     default:
       return 1;
   }
+}
+
+const escapeSymbolsRe = /[\`]/g;
+
+function escapeString(st?: string) {
+  return st && st.replaceAll(escapeSymbolsRe, '\\$&');
+}
+
+function wrapString(st?: string, wrapper = '`') {
+  const escapedString = escapeString(st);
+  return escapedString && `${wrapper}${escapedString}${wrapper}`;
+}
+
+export function getRawSqlFromBuilderOptions(builderOptions: SqlBuilderOptions) {
+  const fields = builderOptions.fields?.map((field) => wrapString(field)).join(', ');
+  const limitCondition = builderOptions.limit ? ` LIMIT ${builderOptions.limit}` : '';
+  return `SELECT ${fields} FROM ${wrapString(builderOptions.table)}${limitCondition}`;
 }
