f9d38e43f911b3f64756eb894e3c3e874babfc26
el-makarova <el-makarova@yandex-team.ru>
2023-06-23 16:23:04 +0300

    feat: select all fields from the table

--- src/components/FieldsSelect.tsx
+++ src/components/FieldsSelect.tsx
@@ -1,7 +1,7 @@
-import { Select, InlineField } from '@grafana/ui';
+import { Select, InlineField, Button } from '@grafana/ui';
 import { SelectableValue } from '@grafana/data';
 
-import { defaultLabelWidth } from 'containers/QueryEditor/constants';
+import { GrafanaFormClassName, defaultLabelWidth } from 'containers/QueryEditor/constants';
 
 import { selectors } from 'selectors';
 
@@ -14,7 +14,7 @@ export type FieldsSelectProps = {
 };
 
 export function FieldsSelect({ onFieldsChange, selectedFields = [], fields, loading, error }: FieldsSelectProps) {
-  const allFields = Array.from(new Set([...fields, ...selectedFields]));
+  const allFields = fields.length > 0 ? Array.from(new Set([...fields, ...selectedFields])) : [];
   const selectableValues = allFields.map((f) => ({ label: f, value: f }));
 
   const { label, tooltip } = selectors.components.QueryBuilder.Fields;
@@ -24,26 +24,31 @@ export function FieldsSelect({ onFieldsChange, selectedFields = [], fields, load
   };
 
   return (
-    <InlineField
-      labelWidth={defaultLabelWidth}
-      tooltip={tooltip}
-      label={label}
-      error={error}
-      invalid={Boolean(error)}
-      grow
-    >
-      <Select
-        // bug in grafana types. isMulti option is not taken into account
-        onChange={handleChange as any}
-        options={selectableValues}
-        value={selectedFields}
-        menuPlacement={'bottom'}
-        allowCustomValue
-        isLoading={loading}
-        isClearable
-        isSearchable
-        isMulti
-      />
-    </InlineField>
+    <div className={GrafanaFormClassName}>
+      <InlineField
+        labelWidth={defaultLabelWidth}
+        tooltip={tooltip}
+        label={label}
+        error={error}
+        invalid={Boolean(error)}
+        grow
+      >
+        <Select
+          // bug in grafana types. isMulti option is not taken into account
+          onChange={handleChange as any}
+          options={selectableValues}
+          value={selectedFields}
+          menuPlacement={'bottom'}
+          allowCustomValue
+          isLoading={loading}
+          isClearable
+          isSearchable
+          isMulti
+        />
+      </InlineField>
+      <Button fill="outline" onClick={() => onFieldsChange(fields)} disabled={fields.length === 0}>
+        All fields
+      </Button>
+    </div>
   );
 }
