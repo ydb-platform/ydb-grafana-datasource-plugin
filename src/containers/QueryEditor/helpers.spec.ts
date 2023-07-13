42622f29211e03e138f0bf55d7dd676cba815979
el-makarova <el-makarova@yandex-team.ru>
2023-07-13 11:33:38 +0300

    test: add tests to sql-query generation

---
+++ src/containers/QueryEditor/helpers.spec.ts
@@ -0,0 +1,16 @@
+import { escapeAndWrapString } from './helpers';
+
+describe('should properly escapeAndWrapString', () => {
+  it('simple string with backticks', () => {
+    expect(escapeAndWrapString('foo')).toBe('`foo`');
+  });
+  it('simple string with double quotes', () => {
+    expect(escapeAndWrapString('foo', '"')).toBe('"foo"');
+  });
+  it('string with forbidden symbols with backticks', () => {
+    expect(escapeAndWrapString('fo\\o`"')).toBe('`fo\\o\\`"`');
+  });
+  it('string with forbidden symbols with double quotes', () => {
+    expect(escapeAndWrapString('fo\\o`"', '"')).toBe('"fo\\o`\\""');
+  });
+});
