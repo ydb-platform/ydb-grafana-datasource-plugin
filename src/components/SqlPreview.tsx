12beabe192ff39f68e4eaa03ff4e3e42f9a5885c
el-makarova <el-makarova@yandex-team.ru>
2023-09-08 12:30:11 +0300

    feat(SqlPreview): always show full content

--- src/components/SqlPreview.tsx
+++ src/components/SqlPreview.tsx
@@ -1,8 +1,9 @@
-import { InlineField, Input } from '@grafana/ui';
+import { InlineField } from '@grafana/ui';
 
 import { defaultLabelWidth } from 'containers/QueryEditor/constants';
 
 import { selectors } from 'selectors';
+import { styles } from 'styles';
 
 interface SqlPreviewProps {
   rawSql?: string;
@@ -10,8 +11,10 @@ interface SqlPreviewProps {
 export function SqlPreview({ rawSql }: SqlPreviewProps) {
   const { label, tooltip } = selectors.components.QueryBuilder.SqlPreview;
   return (
-    <InlineField labelWidth={defaultLabelWidth} tooltip={tooltip} label={label} grow>
-      <Input value={rawSql} disabled />
+    <InlineField labelWidth={defaultLabelWidth} tooltip={tooltip} label={label} shrink>
+      <div className={styles.Common.rawSqlWrapper}>
+        <span>{rawSql}</span>
+      </div>
     </InlineField>
   );
 }
